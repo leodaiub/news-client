1e6275f701c9d9a81963ec8b605cd91f05b55a1e
leodaiub <leodaiub@gmail.com>
2023-06-10 18:48:08 +0200

    auth fix

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -4,8 +4,6 @@ export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "X-Requested-With": "XMLHttpRequest",
-    withCredentials: true,
-    credentials: "include",
   },
   withCredentials: true,
 });
